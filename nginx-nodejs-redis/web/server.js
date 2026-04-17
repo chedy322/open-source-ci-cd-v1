54c678fdad8da8079441210c1b0cd567d128bce3
chedi bououn <chbouountito@gmail.com>
2026-04-17 17:58:35 +0300

    Feat: Add CD repository dispatch

--- nginx-nodejs-redis/web/server.js
+++ nginx-nodejs-redis/web/server.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
         if (isNaN(numVisitsToDisplay)) {
             numVisitsToDisplay = 1;
         }
-        console.log("HELLO WORLD")
+        console.log("HELLO WORLD ffff")
        res.status(200).send(os.hostname() +': Number of visits is: ' + numVisitsToDisplay);
         numVisits++;
         redisClient.set('numVisits', numVisits);
